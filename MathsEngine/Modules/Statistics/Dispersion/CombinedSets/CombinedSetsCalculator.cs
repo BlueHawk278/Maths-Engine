c1f7ade0b387110c996db155ca76913764cf6239
Harry <harryseymour.ulster@gmail.com>
2025-12-12 20:49:32 +0000

    Standard Deviation: Started to combine data sets to calculate the new mean and standard deviation

---
+++ MathsEngine/Modules/Statistics/Dispersion/CombinedSets/CombinedSetsCalculator.cs
@@ -0,0 +1,28 @@
+﻿using MathsEngine.Core.Menu;
+
+namespace MathsEngine.Modules.Statistics.Dispersion.CombinedSets
+{
+    public class CombinedSetsCalculator
+    {
+        private int _numFirstSetPoints;
+        private double mean1, standardDeviation1, sigmaX1, sigmaXSq1;
+
+        private int _numSecondSetPoints;
+        private double Mean2, standardDeviation2, sigmaX2, sigmaXSq2;
+
+        public CombinedSetsCalculator()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
+
+        private void checkFirstDataSet(int numPoints, double mean1, double standardDeviation1, double sigmaX1, double sigmaXSq1)
+        {
+
+        }
+    }
+}
---
+++ MathsEngine/Modules/Statistics/Dispersion/CombinedSets/CombinedSetsInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathsEngine.Modules.Statistics.Dispersion.CombinedSets
+{
+    public class CombinedSetsInput
+    {
+        public static void Start()
+        {
+            Console.Clear();
+            getDataSets();
+
+            //var calculator = new ArrayOfNumbersCalculator(originalValues);
+            //calculator.Run();
+
+            //calculator.DisplayData();
+
+            Console.WriteLine("\nPress Enter to return to the menu.");
+            Console.ReadLine();
+        }
+
+        private static void getDataSets()
+        {
+            Console.Write("How many numbers in the First Data set: ");
+            string numDataPoints1 = Console.ReadLine();
+            Console.Write("What is the mean of the values: ");
+            string mean1 = Console.ReadLine();
+            Console.Write("What is the standard deviation: ");
+            string standardDeviation1 = Console.ReadLine();
+            List<string> dataSet1 = new List<string>();
+
+            Console.Write("How many numbers in the Second Data set: ");
+            string numDataPoints2 = Console.ReadLine();
+            Console.Write("What is the mean of the values: ");
+            string mean2 = Console.ReadLine();
+            Console.Write("What is the standard deviation: ");
+            string standardDeviation2 = Console.ReadLine();
+            List<string> dataSet2 = new List<string>();
+        }
+    }
+}
