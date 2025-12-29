2ea0273ef804bd320db7537d6dbdfd7c8d0f4205
Harry <harryseymour.ulster@gmail.com>
2025-12-29 13:52:18 +0000

    Fully implemented Trigonometry tests

--- MathsEngine.Tests/PureTests/TrigonometryTests/CalculateMissingAngleTests.cs
+++ MathsEngine.Tests/PureTests/TrigonometryTests/CalculateMissingAngleTests.cs
@@ -7,6 +7,22 @@ namespace MathsEngine.Tests.PureTests.TrigonometryTests;
 public class CalculateMissingAngleTests
 {
     /*
+     * VALUE TESTS
+     */
+    [Theory]
+    // SOH (Sine)
+    [InlineData(5, SideType.Opposite, 10, SideType.Hypotenuse, 30)]
+    // CAH (Cosine)
+    [InlineData(8.66, SideType.Adjacent, 10, SideType.Hypotenuse, 30)]
+    // TOA (Tangent)
+    [InlineData(5.77, SideType.Opposite, 10, SideType.Adjacent, 30)]
+    public void CalculateMissingAngle_WithValidInputs_ReturnsCorrectAngle(double side1Length, SideType side1Type, double side2Length, SideType side2Type, double expectedAngle)
+    {
+        var result = Trigonometry.CalculateMissingAngle(side1Length, side1Type, side2Length, side2Type);
+        Assert.Equal(expectedAngle, result, 0);
+    }
+
+    /*
      *  EXCEPTION TESTS
      */
 
--- MathsEngine.Tests/PureTests/TrigonometryTests/CalculateMissingSideTests.cs
+++ MathsEngine.Tests/PureTests/TrigonometryTests/CalculateMissingSideTests.cs
@@ -7,6 +7,28 @@ namespace MathsEngine.Tests.PureTests.TrigonometryTests;
 public class CalculateMissingSideTests
 {
     /*
+     * VALUE TESTS
+     */
+
+    [Theory]
+    [InlineData(10, 30, SideType.Adjacent, SideType.Opposite, 5.77)]
+    [InlineData(10, 30, SideType.Adjacent, SideType.Hypotenuse, 11.55)]
+    [InlineData(10, 45, SideType.Opposite, SideType.Adjacent, 10.0)]
+    [InlineData(10, 45, SideType.Opposite, SideType.Hypotenuse, 14.14)]
+    [InlineData(20, 60, SideType.Hypotenuse, SideType.Opposite, 17.32)]
+    [InlineData(20, 60, SideType.Hypotenuse, SideType.Adjacent, 10.0)]
+    public void CalculateMissingSide_WithValidInputs_ReturnsCorrectResult(
+        double knownSideLength,
+        double angle,
+        SideType knownSideType,
+        SideType sideToFind,
+        double expected)
+    {
+        var result = Trigonometry.CalculateMissingSide(knownSideLength, angle, knownSideType, sideToFind);
+        Assert.Equal(expected, result, 2);
+    }
+
+    /*
      *  EXCEPTION TESTS
      */
 
